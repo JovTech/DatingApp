ab95539a3aca6a38ec2124ff8b87a19ce233c945
JovTech <mokoena.saule@gmail.com>
2022-03-30 02:10:54 +0200

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,12 +35,17 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
             services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
             services.AddIdentityServices(_config);
             
         }
@@ -58,7 +63,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            
             app.UseAuthentication();
 
             app.UseAuthorization();

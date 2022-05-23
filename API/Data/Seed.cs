80742cb46a456294bf2947100b1f1d58a4279ba3
SimphiweMabuza <2016363729@ufs4life.ac.za>
2022-05-23 11:08:34 +0200

    Assingment part 1.1

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,11 +1,25 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 
+    
+          
+            
+    
+
+          
+          
+            
+    
+
+          
+    
+    @@ -32,6 +33,7 @@ public class Seed
+  
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 namespace API.Data
 {
     public class Seed
@@ -13,20 +27,39 @@ namespace API.Data
         public static async Task SeedUsers(UserManager<AppUser> userManager, 
             RoleManager<AppRole> roleManager)
         {
-             if (await userManager.Users.AnyAsync()) return;
-
+            if (await userManager.Users.AnyAsync()) return;
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             if (users == null) return;
-            
-
             var roles = new List<AppRole>
             {
                 new AppRole{Name = "Member"},
                 new AppRole{Name = "Admin"},
                 new AppRole{Name = "Moderator"},
             };
-            
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(UserManager<AppUser> userManager, 
+            RoleManager<AppRole> roleManager)
+        {
+            if (await userManager.Users.AnyAsync()) return;
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            if (users == null) return;
+            var roles = new List<AppRole>
+            {
+                new AppRole{Name = "Member"},
+                new AppRole{Name = "Admin"},
+                new AppRole{Name = "Moderator"},
+            };
             foreach (var role in roles)
             {
                 await roleManager.CreateAsync(role);
@@ -34,17 +67,35 @@ namespace API.Data
 
             foreach (var user in users)
             {
-
+                user.Photos.First().IsApproved = true;
                 user.UserName = user.UserName.ToLower();
                 await userManager.CreateAsync(user, "Pa$$w0rd");
                 await userManager.AddToRoleAsync(user, "Member");
-            }
 
+    
+          
+            
+    
+
+          
+    
+    
+  
+            }
             var admin = new AppUser
             {
                 UserName = "admin"
             };
-
+            await userManager.CreateAsync(admin, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
+        }
+    }
+}
+            }
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
             await userManager.CreateAsync(admin, "Pa$$w0rd");
             await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
         }
